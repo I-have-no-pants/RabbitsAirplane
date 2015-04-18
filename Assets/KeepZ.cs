7f56440e2caef1e148f6f0c2fd3a88d567a7cfc9
Jesper Tingvall <jesper@tingvall.pw>
2015-04-18 09:52:10 +0200

    Audio responce system

---
+++ Assets/KeepZ.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepZ : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var v = transform.rotation.eulerAngles;
+		v.z = 0;
+		var r = transform.rotation;
+		r.eulerAngles = v;
+		transform.rotation = r;
+	}
+}
---
+++ Assets/Plane2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody))]
+public class Plane2D : MonoBehaviour {
+	public Vector3 speed;
+	public float speedMax;
+
+	private Rigidbody m_Rigidbody;
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
