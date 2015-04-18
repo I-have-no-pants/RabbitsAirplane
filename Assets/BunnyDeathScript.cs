39088d62c1e13191138e4a69a45febfe9d5c5f3a
Jesper Tingvall <jesper@tingvall.pw>
2015-04-18 10:56:20 +0200

    Add bullet system

---
+++ Assets/BunnyDeathScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnyDeathScript : MonoBehaviour {
+
+
+	public GameObject ExplosionPref;
+
+	public float DeathTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OnDeath() {
+		Destroy (gameObject, DeathTime);
+		Instantiate (ExplosionPref, transform.position, transform.rotation); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/PlayerGun.cs
+++ Assets/PlayerGun.cs
@@ -9,9 +9,34 @@ public class PlayerGun : MonoBehaviour {
 	public float MaxRotation;
 	public TurretScript turret;
 
+
+	public Vector3 limitAngles;
+
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	float ClampAngle(float angle, float min, float max) {
+		
+		if (angle<90 || angle>270){       // if angle in the critic region...
+			if (angle>180) angle -= 360;  // convert all angles to -180..+180
+			if (max>180) max -= 360;
+			if (min>180) min -= 360;
+		}    
+		angle = Mathf.Clamp(angle, min, max);
+		if (angle<0) angle += 360;  // if angle negative, convert to 0..360
+		return angle;
+	}
+
+	public Vector3 limitAngle(Vector3 v, Vector3 l) {
+		if (l.x != -1)
+			v.x = ClampAngle(v.x, -v.x, v.x);
+		if (l.y != -1)
+			v.y = ClampAngle(v.y, -v.y, v.y);
+		if (l.z != -1)
+			v.z = ClampAngle(v.z, -v.z, v.z);
+		return v;
 	}
 	
 	void FixedUpdate () {
@@ -22,6 +47,9 @@ public class PlayerGun : MonoBehaviour {
 		// Rotate
 		if (target != null) {
 			transform.rotation = Quaternion.RotateTowards( transform.rotation, Quaternion.LookRotation( target.transform.position- transform.position, new Vector3(0,1,0)), MaxRotation * Time.fixedDeltaTime);
+
+
+
 		}
 
 		if (Input.GetButton("Fire1") && timer <= 0) {
